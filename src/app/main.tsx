fab49c7a236dc4a5bdd651e2a3960416c9690d44
Daria Serebriakova <[  ]>
2025-11-28 23:39:04 +0200

    fix

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,11 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router"
 import "../styles/index.css"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-export default function Root() {
-  return <RouterProvider router={router} />
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
